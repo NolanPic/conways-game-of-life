2d09f6cd6328edf763a2da1b76624d27c3d1ec05
Nolan Picini <nolanpic@gmail.com>
2020-05-28 17:28:05 -0700

    style controls

---
+++ src/components/Btn.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const styles = {
+  padding: "0 1rem",
+  backgroundColor: "rgb(88, 128, 181)",
+  fontFamily: "VT323",
+  fontSize: "1.5rem",
+};
+
+const Btn = ({ onClick, children }) => {
+  return (
+    <button style={styles} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Btn;
